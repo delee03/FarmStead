0c0a04204e64a3ef1d596276e194c6844738eacd
Pham Phat <phamphat2003vn@gmail.com>
2024-06-04 01:33:05 +0700

    edit header create a Event Fixedtop for header

--- js/main.js
+++ js/main.js
@@ -4,13 +4,35 @@ window.onscroll = function () {
 
 function myFunction() {
     // var nav = document.getElementById("navbarScroll");
-    var nav = document.querySelector("nav");
+    var header = document.querySelector("header");
+    var search = document.getElementById("faSearch");
+    var mode = document.getElementById("mode");
+    var dropdown = document.getElementById("dropdown");
     if (document.documentElement.scrollTop > 100) {
         console.log("đang scroll");
         //nav.classList.remove("bg-transparent");
-        nav.classList.toggle("bg");
+        header.classList.add("bg-head");
+        search.classList.add("search2");
+        mode.classList.add("mode2");
+        dropdown.style.color = "#040a17";
+        document.querySelector(".navbar-brand").style.color = "#000";
+        //nav.style.backgroundColor = "#ccc !important";
     } else {
-        nav.classList.toggle("bg");
+        header.classList.remove("bg-head");
+        search.classList.remove("search2");
+        mode.classList.remove("mode2");
+        dropdown.style.color = "#fff";
+        document.querySelector(".navbar-brand").style.color =
+            "var(--primary-color)";
         console.log("Không scroll nữa");
     }
 }
+
+document.querySelectorAll("a.dropdown-toggle").forEach(function (element) {
+    element.addEventListener("click", function () {
+        // Xóa thuộc tính 'data-poper-static' từ the ul
+        document
+            .querySelector(".dropdown-menu")
+            .removeAttribute("data-bs-popper");
+    });
+});
